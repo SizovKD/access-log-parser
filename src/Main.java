2790c96578e87d90e80206f4e3794b59ab9de3cd
Kirill Sizov <git.user.k.d.sizov@gmail.com>
2023-05-15 20:18:19 +0300

    Задание 2 по теме Основы Java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-
+        System.out.println("Введите 1-е число и нажмите <Enter>: ");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите 2-е число и нажмите <Enter>: ");
+        int y = new Scanner(System.in).nextInt();
+        String sum = "Сумма:";
+        String difference = "Разность:";
+        String product = "Произведение:";
+        String quotient = "Частное:";
+        System.out.println(sum+(x+y));
+        System.out.println(difference+(x-y));
+        System.out.println(product+(x*y));
+        System.out.println(quotient+((double) x/y));
     }
 }
