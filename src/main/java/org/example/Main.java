22b1a9d552b1e407b38c8f23e36f4cbe5b355004
Kirill Sizov <kirill.dmtrvch.sizov.5858@gmail.com>
2026-01-13 22:42:53 +0300

    Calculator added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма = " + sum);
+        System.out.println("Разность = " + diff);
+        System.out.println("Произведение = " + product);
+        System.out.println("Частное = " + quotient);
+
     }
 }
