8282b5ee24adf4095d9c7c1be780ac8c0e0cff7f
Kirill Sizov <kirill.dmtrvch.sizov.5858@gmail.com>
2026-01-26 14:16:00 +0300

    Cycles done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,32 @@
 package org.example;
 
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        String path;
+        File file;
+        boolean fileExists;
+        boolean isDirectory;
+        int i = 1;
+        while (true) {
+            System.out.println("Введите путь:");
+            path = new Scanner(System.in).nextLine();
+            file = new File(path);
+            isDirectory = file.isDirectory();
+            fileExists = file.exists();
+            if (isDirectory) {
+                System.out.println("Указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            if (fileExists) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + i++);
+            } else {
+                System.out.println("Указанного файла не существует или путь указан некорректно");
+            }
+
+        }
     }
 }
